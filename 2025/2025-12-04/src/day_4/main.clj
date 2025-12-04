4379490a6979c2bfa5fe8104511f711e9e532a70
Eivin Landa <eivin.landa@outnordic.com>
2025-12-04 14:03:45 +0100

    day 4 part 2

--- 2025/2025-12-04/src/day_4/main.clj
+++ 2025/2025-12-04/src/day_4/main.clj
@@ -35,38 +35,64 @@
           (range width) row))
       (range height) matrix)))
 
-(defn summarizee
-  [matrix]
+(defn summarize [matrix]
+  (->> matrix
+       (mapcat identity)
+       (filter some?)
+       (map #(reduce + 0 %))
+       (filter #(< % 4))
+       (count)))
+
+(defn get-removals [matrix]
   (->> matrix
        (mapv (fn [row]
                (mapv (fn [adjacents]
-                       (when adjacents
-                         (reduce + 0 adjacents))) row)))
-       (reduce (fn [result row]
-                 (->> row
-                      (filter #(and (some? %) (> 4 %)))
-                      (count)
-                      (+ result)))
-               0)))
-(defn summarize [matrix]
-  (->> matrix
-       (mapcat identity)                       ; flatten to 1d seq of adjacents vecs
-       (filter some?)                          ; drop nils, if any
-       (map #(reduce + 0 %))                   ; sum each
-       (filter #(< % 4))                       ; only sums < 4
-       (count)))                               ; count them
+                       (if (and (some? adjacents)
+                                (> 4 (reduce + 0 adjacents)))
+                         1 0)) row)))))
+(defn count-removals [matrix]
+  (reduce + 0 (flatten matrix)))
 
-(defn solve [file]
+
+(defn solve-part-1 [file]
   (->> file
        (slurp)
        (parse)
        (transform)
        (adjacent-cells)
-       (summarize)))
+       (get-removals)
+       (count-removals)))
 
-(println
- "\nTest\t  "
- (solve "./test_input.txt")
- "\nActual\t"
- (solve "./input.txt"))
+(defn apply-removals [grid removals]
+  (mapv (fn [row-grid row-rem]
+          (mapv (fn [val-grid val-rem]
+                  ;; If the removal matrix says 1, set cell to 0. 
+                  ;; Otherwise, keep the existing value.
+                  (if (= 1 val-rem) 0 val-grid))
+                row-grid row-rem))
+        grid removals))
+
+(defn solve-part-2 [file]
+  (let [grid (->> file
+                           (slurp)
+                           (parse)
+                           (transform))]
+    ;; Loop tracks the CURRENT grid state
+    (loop [current-grid grid
+           total-removed 0]
+      (let [matrix (adjacent-cells current-grid)
+            removals   (get-removals matrix)
+            n          (count-removals removals)]
+        
+        (if (zero? n)
+          total-removed
+          ;; RECUR: Pass the NEW grid and increment round
+          (recur (apply-removals current-grid removals)
+                 (+ total-removed n)))))))
+
+(println ""
+         (solve-part-1 "./test_input.txt") "\n"
+         (solve-part-1 "./input.txt") "\n"
+         (solve-part-2 "./test_input.txt") "\n"
+         (solve-part-2 "./input.txt"))
 
