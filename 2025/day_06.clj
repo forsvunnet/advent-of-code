d93a3ea38c52d3ef7718277a29f27b6d35e8ab7b
Eivin Landa <eivin.landa@outnordic.com>
2025-12-07 11:54:47 +0100

    day 6 part 1

--- 2025/day_06.clj
+++ 2025/day_06.clj
@@ -1,26 +1,40 @@
 (ns day-06
   (:require [clojure.string :as str]))
 
+(defn lines-to-numbers [lines]
+  (mapv (fn [line] (mapv parse-long line)) lines))
+
 (defn parse [content]
   ;; Split into lines
   (->> (str/split content #"\n")
        ;; Split each line into words
-       (mapv (fn [line] (str/split line #" ")))
+       (mapv (fn [line] (str/split (str/trim line) #"\s+")))
        ;; now we have a vector of vectors
        ;; last line is a vector of operators
        ;; rest are vectors of numbers
-       ;; split into a map of :numbers and :operators
-       ((fn [lines]
-          {:numbers (butlast lines)
-           :operators (last lines)}))))
+       ;; split into a map of :table and :operators
+       ((fn [lines] {:table (lines-to-numbers (butlast lines))
+                     :operators (last lines)}))))
 
+(defn process [{:keys [table operators]}]
+  (mapv (fn [[i op]]
+          (let [numbers (mapv #(get % i) table)]
+            (case op
+              "+" (reduce + numbers)
+              "*" (reduce * numbers)
+              "-" (reduce - numbers)
+              op)))
+        (map-indexed vector operators)))
 
 (defn solve-part-1 [file]
   (->> (slurp file)
-       (parse)))
+       (parse)
+       (process)
+       (reduce +)))
+
 
 (println "🧪 Part 1 (Test):" (solve-part-1 "day_06_test.txt"))
-;(println "🧪 Part 1 (Real):" (solve-part-1 "day_06_input.txt"))
+(println "🧪 Part 1 (Real):" (solve-part-1 "day_06_input.txt"))
 
 (defn solve-part-2 [file]
   (->> (slurp file)
